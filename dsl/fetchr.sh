f19338c8b8cc9c44c0a23fdc87db8a968466c14d
Aleksandr Mironov <amironov@level5ops.com>
2018-01-03 20:13:25 +0300

    Update fetchr.sh

--- dsl/fetchr.sh
+++ dsl/fetchr.sh
@@ -14,3 +14,7 @@ if [ "$(docker ps -q -f name=fetchr)" ]; then
 fi
 
 docker run -d --name fetchr -p 8080:8080 --restart=always fetchr
+
+fetchr_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' fetchr)
+
+wait_for.sh -t 1800 $fetchr_ip:8080 -- echo "fetchr is app"
