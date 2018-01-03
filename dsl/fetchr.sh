4b3e765f9108036f29378d0166a52fb037710c12
Aleksandr Mironov <amironov@level5ops.com>
2018-01-03 18:14:08 +0300

    Update fetchr.sh

--- dsl/fetchr.sh
+++ dsl/fetchr.sh
@@ -2,4 +2,15 @@
 cp tools/docker/Dockerfile app/
 cd app && mvn package
 docker build -t fetchr .
-docker run -d -p 8080:8080 fetchr
+
+if [ "$(docker ps -q -f name=fetchr)" ]; then
+	#stop
+	docker stop fetchr
+    if [ "$(docker ps -aq -f status=exited -f name=fetchr)" ]; then
+        # cleanup
+        docker rm fetchr
+    fi
+    # run your container
+fi
+
+docker run -d --name fetchr -p 8080:8080 --restart=always fetchr
